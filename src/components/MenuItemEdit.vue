91811a3dd73bb075cb62c070dea9ed113ec2ef92
Бачой Роман <bachoi.roman@gmail.com>
2020-08-05 11:17:14 +0300

    Форма редактирования переделана на сторонние компоненты Buefy.

--- src/components/MenuItemEdit.vue
+++ src/components/MenuItemEdit.vue
@@ -1,25 +1,39 @@
 ﻿<template>
-    <div style="text-align:center; margin:0 auto">
+    <div>
         <h1>Редактирование блюда</h1>
 
-        <table style="width:50%; margin:0 auto">
-            <tr>
-                <td style="width:20%">Название</td>
-                <td style="width:80%"><input ref="name" type="text" v-model="menuItem.name" style="width:100%"></td>
-            </tr>
-            <tr>
-                <td>Описание</td>
-                <td><input ref="description" type="text" v-model="menuItem.description" style="width:100%"></td>
-            </tr>
-            <tr>
-                <td>Вес, кг</td>
-                <td><input ref="weight" type="number" v-model.number="menuItem.weight" style="width:100%"></td>
-            </tr>
-            <tr>
-                <td>Цена, руб.</td>
-                <td><input ref="price" type="number" v-model.number="menuItem.price" style="width:100%"></td>
-            </tr>
-        </table>
+        <div style="width:50%; text-align:left; margin:0 auto">
+            <b-field label="Название">
+                <b-input v-model="menuItem.name"
+                         ref="name">
+                </b-input>
+            </b-field>
+
+            <b-field label="Описание">
+                <b-input v-model="menuItem.description"
+                         ref="description">
+                </b-input>
+            </b-field>
+
+            <b-field label="Вес, гр.">
+                <b-input type="number"
+                         min="0"
+                         max="100000"
+                         v-model.number="menuItem.weight"
+                         ref="weight">
+                </b-input>
+            </b-field>
+
+            <b-field label="Цена, руб.">
+                <b-input type="number"
+                         min="0"
+                         max="100000"
+                         v-model.number="menuItem.price"
+                         ref="price">
+                </b-input>
+            </b-field>
+
+        </div>
         <br /><br />
         <div style="text-align:center; width: 50%; margin:0 auto">
             <input type="button" value="Сохранить" style="margin-right:30px" v-on:click="save()"/>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
 axios.defaults.baseURL = 'http://localhost:3000';
 
 Vue.config.productionTip = false
